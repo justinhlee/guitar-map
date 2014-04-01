461d755c673c8f2895e97f83b27b9e86dddcd7dd
Justin Lee <lee.justin.h@gmail.com>
2014-04-01 11:56:05 -0400

    More logic organization

--- js/main.js
+++ js/main.js
@@ -4,63 +4,119 @@
 'use strict';
 
 $(function() {
-  var guitar = Backbone.Model.extend({
-    
+  // Constructor for a finger position
+  function Position(fretNo, stringNo) {
+    this.fretNo   = fretNo;
+    this.stringNo = stringNo;
+  }
+
+  // Chords have a name and a list of finger positions
+  var Chord = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        name: 'no chord',
+        // Position of root note on E string
+        root: 0,
+        positions: []
+      };
+    }
   });
 
-  var guitarView = Backbone.View.extend({
+  var ChordView = Backbone.View.extend({
     el: $('canvas'),
 
     initialize: function() {
       this.render();
     },
+
+    render: function() {
+      var canvas = this.el, ctx = canvas.getContext('2d');
+      var notes = this.model.get('positions');
+    
+      // Iterate through every note of a chord and draw it
+      for (var i = 0; i < notes.length; i++) {
+        var fret = notes[i].fretNo;
+        var str  = notes[i].stringNo;
+        drawNote(ctx, fret, str);
+      }
+    }
+  });
+
+  var ChordProgression = Backbone.Collection.extend({
+    model: Chord
+  });
+
+  var GuitarView = Backbone.View.extend({
+    el: $('canvas'),
+
+    initialize: function() {
+      this.render();
+      /* CHANGE BELOW LINE 
+
+      */
+      var p1 = new Position(1,5);
+    
+      var p3 = new Position(2,3);
+      var ps = [p1,p3];
+      this.addChord(new Chord({
+        name: 'Am7',
+        positions: ps
+      }));
+    },
     
     render: function() {
       var canvas = this.el, ctx = canvas.getContext('2d');
       drawStrings(ctx);
-      drawNote(ctx, 1, 5);
-      drawNote(ctx, 2, 4);
-      drawNote(ctx, 2, 3);
-  
-      ctx.fillStyle = 'rgba(0, 0, 0, .5)';
-      drawNote(ctx, 5, 5);
-      drawNote(ctx, 5, 4);
-      drawNote(ctx, 5, 3);
-      drawNote(ctx, 5, 1);
+
+    },
+
+    addChord: function(toBeAdded) {
+      var addedChord = new ChordView({model: toBeAdded});
     }
   });
 
-  var app = new guitarView;
+  var app = new GuitarView;
 });
 
 
+/* 
+* Helper functions for drawing 
+*/
 
+var fretDistance   = 75;
+var stringDistance = 35;
 
 function drawStrings(fretboard) {
   for (var i = 0; i < 6; i++) {
     fretboard.strokeStyle = '#E36B2A';
     fretboard.beginPath();
-    fretboard.moveTo(0, i*25+1);
-    fretboard.lineTo(1000, i*25+1);
+    fretboard.moveTo(0, i*stringDistance+1);
+    fretboard.lineTo(1000, i*stringDistance+1);
     fretboard.closePath();
     fretboard.stroke();
   }
-  for (var i = 0; i < 12; i++) {
+  for (var i = 0; i < 13; i++) {
     fretboard.strokeStyle = 'gray';
     fretboard.lineWidth = 3;
     fretboard.beginPath();
-    fretboard.moveTo(i*60+1, 0);
-    fretboard.lineTo(i*60+1, 125);
+    fretboard.moveTo(i*fretDistance+1, 0);
+    fretboard.lineTo(i*fretDistance+1, stringDistance * 5);
     fretboard.closePath();
     fretboard.stroke();
+    if (i === 3 || i === 5 || i === 7 || i === 12) {
+      var x = i * fretDistance - (fretDistance/2 - 1);
+      var y = stringDistance * 5 + 20;
+
+      // Put fret markers
+    }
   }
 }
 
 function drawNote(fretboard, fretNo, stringNo) {
   stringNo = 6 - stringNo;
   fretboard.beginPath();
-  var x = fretNo*60-29;
-  var y = stringNo*25+1;
+  var x = fretNo*fretDistance-(fretDistance/2 - 1);
+  var y = stringNo*stringDistance+1;
   var radius = 10;
   var startAngle = 0;
   var endAngle = Math.PI*2;
